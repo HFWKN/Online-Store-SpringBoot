a18fa15fad6130e784c821a5015364fab72a748f
LBQ <1353823108@qq.com>
2026-08-01 22:55:30 +0800

    refactor(seckill): 更改了一些注释

--- store-seckill/src/main/java/com/liubingqi/seckill/service/impl/StockOrderServiceImpl.java
+++ store-seckill/src/main/java/com/liubingqi/seckill/service/impl/StockOrderServiceImpl.java
@@ -51,8 +51,12 @@ public class StockOrderServiceImpl implements IStockOrderService {
             return CodeInfoUtils.of(Result_Code.USER_NOT_LOGIN);// 用户未登录
         }
         // 实际使用: SECKILL_DUPLICATE_ORDER_KEY_PREFIX + activityId + ":" + productId + ":" + specId + ":" + userId
+
+        // 防重复下单key  //  拼接 Key：包含了活动ID、商品ID、规格ID和用户ID。这说明：这个 Token 是这个用户、针对这个商品的专属凭证。
+        // 用户下单的时候，前端会请求后端的getToken方法，生成本次下单的唯一token，getToken方法会将此Token存入redis
         String key1 = SeckillRedisKeyConstants.SECKILL_DUPLICATE_ORDER_KEY_PREFIX
                 + dto.getActivityId() + ":" + dto.getProductId() + ":" + dto.getSpecId() + ":" + userId;
+        // 如果没有这个Token，就说明本次下单不是正常途径的。
         String token = stringRedisTemplate.opsForValue().get(key1);
         // 判断获取的token是否为空
         if (token == null) {
@@ -70,6 +74,7 @@ public class StockOrderServiceImpl implements IStockOrderService {
         Long productId = dto.getProductId();
         Long activityId = dto.getActivityId();
 
+        // 这个key是判断该用户是否购买过某个商品的某个规格。限购用
         String buyKey = SeckillRedisKeyConstants.SECKILL_BUY_COUNT_KEY_PREFIX
                 + activityId + ":" + productId + ":" + specId + ":" + userId;
         // 获取当前活动过期时间
@@ -78,24 +83,25 @@ public class StockOrderServiceImpl implements IStockOrderService {
             return CodeInfoUtils.of(Result_Code.ACTIVITY_END);// 活动已结束
         }
 
+        // 判断是否有这个key(也就是判断该用户有没有重复购买同一个规格的商品)
         Boolean b = stringRedisTemplate.opsForValue().setIfAbsent(buyKey, "lbqsayhello", ttlSeconds, TimeUnit.SECONDS);
-        // 判断是否有这个key
         if (Boolean.FALSE.equals(b)) {
             return CodeInfoUtils.of(Result_Code.REPEAT_PURCHASES);// 已购买
         }
         // 用户未购买，执行下单操作
         // 获取要扣减库存的key
         // SECKILL_NUM_KEY_PREFIX + activityId:productId:specId
+        // 该key为某一商品库存Key
         String key = SeckillRedisKeyConstants
                 .SECKILL_NUM_KEY_PREFIX + dto.getActivityId() + ":" + dto.getProductId() + ":" + dto.getSpecId();
 
         long remainStock;
 
         try {
-            // 扣减库存-decrement是原子性操作
+            // 扣减库存(库存-1)-(decrement)是原子性操作
             remainStock = stringRedisTemplate.opsForValue().decrement(key);
         }catch (Exception e){
-            // 扣库存异常时回滚限购占位，允许用户重试
+            // 扣库存异常时回滚限购占位，允许用户重试(删除该用户对于某一商品的限购)
             stringRedisTemplate.delete(buyKey);
             return CodeInfoUtils.of(Result_Code.FAIL);// 下单失败，重新点击购买试试呢^_^。前端需要重新获取token
         }
@@ -126,30 +132,17 @@ public class StockOrderServiceImpl implements IStockOrderService {
                     .setCreateTime(LocalDateTime.now())
                     .setUpdateTime(LocalDateTime.now());
 
-/*            // 2. 扣减成功
-            // 发送消息给MQ
-            outboxService.save(outbox);
             try {
-                // 发送消息
-                //mqSender.sendCreateOrderMessage(message);
-            }catch (Exception e){
-                // 如果发送消息失败，把库存加回去
-                stringRedisTemplate.opsForValue().increment(key);
-                // 发送失败回滚限购占位，允许用户重试
-                stringRedisTemplate.delete(buyKey);
-                return CodeInfoUtils.of(Result_Code.FAIL);// 下单失败，重新点击购买试试呢^_^ .前端重新获取token
-            }*/
-
-            try {
+                // 将消息添加到本地信息表(outbox表)
                 outboxService.save(outbox);
             } catch (Exception e) {
                 // 写 Outbox 失败，回滚 Redis 库存和限购占位，允许用户重试
                 stringRedisTemplate.opsForValue().increment(key);
                 stringRedisTemplate.delete(buyKey);
-                return CodeInfoUtils.of(Result_Code.FAIL);
+                return CodeInfoUtils.of(Result_Code.FAIL);// 下单失败，重新点击购买试试呢^_^
             }
-            // 扣减成功，返回成功结果
-            return CodeInfoUtils.of(Result_Code.SUCCESS); // 下单成功
+            // redis库存扣减且落库本地消息表成功，返回成功结果
+            return CodeInfoUtils.of(Result_Code.SUCCESS); // 已受理，稍后进入订单界面查看订单
         } else {
             // 3. 库存不足，把多扣的那 1 个补回去
             stringRedisTemplate.opsForValue().increment(key);
